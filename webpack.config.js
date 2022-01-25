09d73943fea69278b7bfa5ebcf4f8123eb640755
KCrandall6 <kcrandall67@gmail.com>
2022-01-24 17:37:43 -0700

    boiler plate
    Co-authored-by: Trevor Leung <trevleung@users.noreply.github.com>
    Co-authored-by: yoyoyojoe <yoyoyojoe@users.noreply.github.com>
    Co-authored-by: Trevor <tmow12@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: './client/index.js',
+    output: {
+        path: path.resolve('build'),
+        filename: 'bundle.js',
+    },
+    plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({template: './index.html'})],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use:[{
+                        loader: 'url-loader',
+                        options: {
+                        limit: 8192,
+                        },
+                }]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [ 
+                    MiniCssExtractPlugin.loader,
+                    // "style-loader", // this is only used if we don't use the css plugin
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+        ],
+    },
+    devServer: {
+        compress: true,
+        port: 8080,
+        proxy: {
+            '/': 'http://localhost:3000'
+        }
+    }
+}
