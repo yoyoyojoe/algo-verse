97273c02cf9c9ae28b4b63bba762b0949cd7f0c7
KCrandall6 <kcrandall67@gmail.com>
2022-01-24 19:35:24 -0700

    boiler plate
    Co-authored-by: Trevor Leung <trevleung@users.noreply.github.com>
    Co-authored-by: yoyoyojoe <yoyoyojoe@users.noreply.github.com>
    Co-authored-by: Trevor <tmow12@users.noreply.github.com>

--- client/index.js
+++ client/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
-import App from './App'
-// import styles from './scss/application.scss'
+import { BrowserRouter, Routes, Route } from 'react-dom';
+import App from './App';
+import Login from './Login';
 
-render(
-    <App />,
+
+ReactDOM.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>,
     document.getElementById('root')
-)
+);
