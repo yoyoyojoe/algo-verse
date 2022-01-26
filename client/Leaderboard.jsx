03c76fb524370f81ab79230f238de901f68dce58
KCrandall6 <kcrandall67@gmail.com>
2022-01-25 20:05:57 -0700

    added functionality to questionaire

--- client/Leaderboard.jsx
+++ client/Leaderboard.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Leaderboard = (props) => {
+
+  const [leaders, getLeaderboard] = useState({
+    username: '',
+    score: '',
+  })
+
   return (
     <div>
       <h1>In the Leaderboard</h1>
--- client/Questionaire.jsx
+++ client/Questionaire.jsx
@@ -1,34 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Questionaire = (props) => {
 
-  function handleAnswer() {
-    const newAnswer = {
-      timeMessage: timeMessage,
-      spaceMessage: spaceMessage,
-    }
-    fetch('/checksubmission', {
-        method: "POST",
-        body: newAnswer,
-        headers: { "Content-Type": "application/json" },
-    })
-    .then((res) => res.json())
-    .then((res) => 
-    // check if answer is correct,
-      // if so, 
-        // change answer variable to true
-        // fetch a new question
-      // else, 
-        // change answer variable to incorrect
-        // send the count and username to leaderboard table (backend)
-        // reset the count
-        // prompt a window to try again?
-          // reload the page
-    console.log(res));
-  }
-
   const [question, getQuestion] = useState({
-    questionName: '',
     question: '',
     answer: '',
     difficulty: '',
@@ -36,26 +11,73 @@ const Questionaire = (props) => {
     spaceComplexity: ''
   });
 
-  const [leaders, getLeaders] = useState([]);
 
-  const [count, setCount] = useState(0);
+
+
+  const getNewQuestion = (() => {
+    axios.get('http://localhost:3000/getNewQuestion')
+    .then((res) => {
+      getQuestion(res.data.events)
+    })
+  })
+
+  const postToLeaderboard = ((score) => {
+    axios({ method: 'POST', url: 'http://localhost:3000/toleaderboard', data: score })
+    .then((res) => {
+      console.log(res);
+    });
+  })
+
+
+  function handleAnswer() {
+    const newAnswer = {
+      timeMessage: document.getElementById('timeInput').value,
+      spaceMessage: document.getElementById('timeInput').value,
+    }
+    // check if answer is correct,
+      if (newAnswer.spaceMessage === res.data.events.spaceComplexity && newAnswer.timeMessage === res.data.events.timeComplexity) {
+        // if so, 
+          // change answer variable to true
+
+          // fetch a new question
+          getNewQuestion();
+      } else {
+        // else, 
+        // change answer variable to incorrect
+        // send the count and username to leaderboard table (backend)
+        postToLeaderboard();
+        // reset the count
+        // prompt a window to try again?
+          // reload the page
+      }
+    };
+
+
+
+  const [score, setScore] = useState(0);
 
   const [answer, setAnswer] = ["?"]
 
-{/* <button onClick={() => setCount(count + 1)}></button> */}
   return (
     <div>
-      <div className="Num-diff-display">
-        <p>Question#: </p>
-        <p>Difficulty: </p>
+      <div>
+        <button  
+          className="getAQuestion"
+          onClick={() => {getNewQuestion()}}
+        >Get a Question</button>
       </div>
-      <p className="question"></p>
-      <p className="questionAnswer"></p>
+      <div className="Num-diff-display">
+        <p>Question#: {score}</p>
+        <p>Difficulty: {res.data.events.difficulty}</p>
+      </div>
+      <p className="question"> Question: {res.data.events.question}</p>
+      <p className="questionAnswer">Question answer: {res.data.events.answer}</p>
       <p className="instructions">
         this is how you answer the question: ...
       </p>
       <input
       className="timeInput"
+      id="timeInput"
       placeholder="what is your time answer?"
       // onChange={(e) => (timeMessage = e.target.value)}
       // value={submission}
@@ -63,13 +85,17 @@ const Questionaire = (props) => {
       </input>
       <input
       className="spaceInput"
+      id="spaceInput"
       placeholder="what is your space answer?"
-      onChange={(e) => (spaceMessage = e.target.value)}
+      // onChange={handleChange}
       // value={submission}
       >
       </input>
       <div className="answerbox">Your Answer is... </div>
-      <button >SUBMIT</button>
+      <button 
+        className="submitAnswer"
+        onClick={() => {handleAnswer()}}
+      >SUBMIT</button>
     </div>
   )
 };
