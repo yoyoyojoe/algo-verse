95832eef8debc8ca91b121efef93bacd47f514d7
Trevor Leung <trevortyleung@gmail.com>
2022-01-25 19:14:26 -0800

    leader board end route

---
+++ server/controllers/appControllers.js
@@ -0,0 +1,72 @@
+const db = require('../models/algoVerseModels')
+
+const appController = {};
+
+
+// algoVerseController.difficulty = (req, res, next) => {
+//   console.log('reached get state')
+//   const text = 
+//   `SELECT company, position, summary, core_values FROM public.users u
+//   LEFT OUTER JOIN public.job_listings jl ON u._id = jl.user_id
+//   WHERE progress = 'applied';`
+//   db.query(text)
+//       .then((received) => {
+//           res.locals.listings = received.rows;
+//           return next();
+//       })
+// };w
+
+appController.getNewQuestion = (req, res, next) => {
+    console.log('reached get new questions');
+    const text = 
+    `SELECT * FROM algorithms
+    WHERE question_id = 5;`;
+    db.query(text)
+        .then((received) => {
+          console.log(received.rows);
+          res.locals.algorithm = received.rows;
+          return next();
+        });
+};
+
+appController.getLeaderBoard = (req, res, next) => {
+    const text = 
+    `SELECT username, score FROM users
+    ORDER BY score desc;`
+    db.query(text)
+        .then(received => {
+          console.log('this is from leaderboard', received.rows);
+          res.locals.leaderboard = received.rows;
+          return next();
+        });
+};
+
+// algoVerseController.deleteJobListing = (req, res, next) => {
+//     // const params = [ req.body.level, req.body.id ]
+//     // const text = 
+//     // `DELETE FROM public.job_listings 
+//     // WHERE progress = $1 AND _id ='Alfreds Futterkiste';`
+//     // db.query(text)
+//     //     .then(received => next());
+// }
+
+// algoVerseController.updateJobListing = (req, res, next) => {
+    
+// }
+
+appController.login = (req, res, next) => {
+    console.log('reached login controller');
+    // console.log(req.body);
+    params = [ req.body.username, req.body.password ]
+    text = 
+    `SELECT 1
+    FROM users
+    WHERE username = $1 AND password = $2;`
+    db.query(text, params)
+        .then((receive) => {
+            res.locals.authen = receive.rows;
+            return next();
+        })
+}
+
+module.exports = appController;
---
+++ server/controllers/loginControllers.js
@@ -0,0 +1,35 @@
+const db = require('../models/algoVerseModels')
+
+const loginController = {};
+
+loginController.login = (req, res, next) => {
+  console.log('reached login controller');
+  // console.log(req.body);
+  params = [ req.body.username, req.body.password ]
+  text = 
+  `SELECT 1
+  FROM users
+  WHERE username = $1 AND password = $2;`
+  db.query(text, params)
+      .then((receive) => {
+          res.locals.authen = receive.rows;
+          return next();
+      })
+};
+
+
+
+loginController.createSession = (req, res, next) => {
+    console.log('creating session');
+};
+
+loginController.signUp = (req, res, next) => {
+  console.log('signing up');
+  const [ username, password ] = req.body;
+  const params = [ username, password ];
+  const text = `
+  SELECT * FROM users
+  WEHERE $1 = $2;
+  
+  `
+};
